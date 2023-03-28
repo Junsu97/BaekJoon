77940fa1a974ad4d28d28f926611e498c5d24374
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-03-28 16:58:02 +0900

    .

--- src/main/java/Lv3/BOJ2439/Main.java
+++ src/main/java/Lv3/BOJ2439/Main.java
@@ -1,7 +1,21 @@
 package Lv3.BOJ2439;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int i = 0; i < N; i++){
+            for(int j = (N - (i+1)); j > 0; j--){
+                System.out.print(" ");
+
+            }
+            for(int k = 0; k < (i+1); k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
 }

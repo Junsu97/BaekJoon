0a4e0e2a7bddd41bbcd375e497d6a0302c69401a
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-04-02 17:34:53 +0900

    .

--- src/main/java/Lv3/BOJ2439/Main.java
+++ src/main/java/Lv3/BOJ2439/Main.java
@@ -6,16 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-
-        for(int i = 0; i < N; i++){
-            for(int j = (N - (i+1)); j > 0; j--){
+        String s = "*";
+        for(int i = 0; i < N; i++){    // 줄을 몇개 만들 것인가
+            for(int j = (N - (i+1)); j > 0; j--){ // 공백을 몇개 찍을 것인가.
                 System.out.print(" ");
-
             }
-            for(int k = 0; k < (i+1); k++){
-                System.out.print("*");
-            }
-            System.out.println(" ");
+            System.out.println(s);
+            s+="*";
         }
     }
 }

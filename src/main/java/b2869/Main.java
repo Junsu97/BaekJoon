04b7abf977c74b0570bff788c7845a26cd74dee3
junsu97 <wnstn7759@naver.com>
2024-08-29 12:08:38 +0900

    .

--- src/main/java/b2869/Main.java
+++ src/main/java/b2869/Main.java
@@ -1,24 +1,19 @@
 package b2869;
 
 import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
-        int V = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
         br.close();
-        int count = 0;
-        while (V > 0){
-            count++;
-            V -= A;
-            if(V <= 0){
-                break;
-            }
-            V += B;
-        }
-        bw.write(String.valueOf(count));
+        int days = (V - B - 1) / (A - B) + 1;
+        bw.write(String.valueOf(days));
         bw.flush();
         bw.close();
     }
